9a21766286f5a3f9e630b3932cb4ec1939168689
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2013-11-10 15:47:16 -0500

    Fix case-sensitivty issue

--- TrackHandler.cpp
+++ TrackHandler.cpp
@@ -2,7 +2,7 @@
 // This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License
 // http://creativecommons.org/licenses/by-sa/3.0/deed.en_US
 
-#include "trackhandler.h"
+#include "TrackHandler.h"
 
 #include <QDebug>
 #include <QFileDialog>
